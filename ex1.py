0ec0fbdb1d279995d58dc2963b5055b68bdf6407
hello <hello@hello.westell.com>
2017-07-24 15:10:58 -0700

    Test code

--- ex1.py
+++ ex1.py
@@ -6,15 +6,25 @@
 from random import randint
 
 def main():
-    print "Welcome to the number guessing game!"
+    print "Welcome to the number guessing game! You get five chances."
     number = randint(1, 10)
+    tries = 0
+    
     while True:
         guess = raw_input("Guess a number between one and ten: ")
+        
         if int(guess) == number:
             print "That's right!"
             break
         else:
-            print "That's not right. Sorry! Guess again."
+            tries = tries + 1
+            attempts = 5 - tries
+            if tries == 5:
+                print "That's not right. Sorry! Game over."
+                print "My number was {}.".format(number)
+                break
+            else:
+                print "That's not right. Sorry! You have {} attempts left.".format(attempts)
 
 if __name__ == '__main__':
     main()
